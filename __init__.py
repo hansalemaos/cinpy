4908dac5a321d3c5fc84466650540030f1e3da1f
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-02-24 19:40:56 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import collections
 import configparser
+import itertools
 import os
 import re
 import subprocess
@@ -201,10 +202,314 @@ d_typesall.append(
         alias="Complex number type composed of 2 128-bit extended-precision floating-point numbers",
     )
 )
+d_typesall.append(
+    d_types2(
+        np="np.int32",
+        c="size_t",
+        ct="ctypes.c_size_t",
+        use=True,
+        comment="for size",
+        code="",
+        alias="for size",
+    )
+)
+
+
+def compile_cpp_codev2(
+    outputdll,
+    cfgfile,
+    cpath,
+    modulename,
+    vcvarsall_bat,
+    cl_exe,
+    link_exe,
+    compilerflags,
+    allfunctionvariations,
+):
+    try_delete_file(outputdll)
+    try_delete_file(cfgfile)
+    try_delete_file(outputdll[:-3] + "exp")
+    try_delete_file(outputdll[:-3] + "lib")
+    try_delete_file(cpath[-3] + "obj")
+    compile_cppv2(
+        modulename=modulename,
+        fnames=allfunctionvariations,
+        vcvarsall_bat=vcvarsall_bat,
+        cl_exe=cl_exe,
+        link_exe=link_exe,
+        cppsource=cpath,
+        compilerflags=compilerflags,
+    )
+
+
+def compile_cppv2(
+    modulename,
+    fnames,
+    vcvarsall_bat,
+    cl_exe,
+    link_exe,
+    cppsource,
+    compilerflags=(
+        "/std:c++20",
+        "/Ferelease",
+        "/EHsc",
+        "/MT",
+        "/O2",
+        "/bigobj",
+    ),
+):
+    cfgfile = get_file(f"{modulename}.ini")
+    output = get_file(f"{modulename}.dll")
+
+    config = configparser.ConfigParser()
+    allcommand = [
+        vcvarsall_bat,
+        "x64",
+        "&&",
+        cl_exe,
+        "/D_USRDL",
+        "/D_WINDLL",
+        cppsource,
+        *compilerflags,
+        "/link",
+        "/DLL",
+        f'/OUT:"{output}"',
+        "/MACHINE:X64",
+    ]
+    subprocess.run(allcommand, shell=True)
+
+    p = subprocess.run([link_exe, "/dump", "/exports", output], capture_output=True)
+    fnamesre = [
+        (
+            x,
+            re.compile(
+                rf"[\r\n]+\s+[a-fA-F0-9]+\s+[a-fA-F0-9]+\s+[a-fA-F0-9]+\s+(\?[^\s]*{x}@[^\s]+)"
+            ),
+        )
+        for x in fnames
+    ]
+    decor = p.stdout.decode("utf-8", "ignore")
+    print(decor)
+    franmesre = [(x[0], x[1].findall(decor)) for x in fnamesre]
+    config["DEFAULT"] = {k: v[0] for k, v in franmesre if v}
+    with open(cfgfile, "w") as f:
+        config.write(f)
+
+
+def iter_nested_for_loop(*args):
+    for vals in itertools.product(*args):
+        yield vals
+
+
+def write_source_code_filesv2(
+    modulename, var_variations, dtypemustbeequal, fu, cfunctioname, cheader, cfooter
+):
+    cfgfile = get_file(f"{modulename}.ini")
+    outputdll = get_file(f"{modulename}.dll")
+    cpath = get_file(f"{modulename}_cppcode.cpp")
+    pypath = get_file(f"{modulename}module.py")
+
+    allvari = []
+    for q in list(iter_nested_for_loop(*[x[1] for x in var_variations.items()])):
+        allvari.append({qq: k for qq, k in zip(list(var_variations.keys()), q)})
+
+    allvarifinal = []
+    for v in allvari:
+        checkva = []
+
+        for d in dtypemustbeequal:
+            for dd in d:
+                checkva.append(v.get(dd))
+            print(checkva)
+        if len(set(checkva)) > 1:
+            continue
+        allvarifinal.append(v)
+
+    allinfos = []
+    for v in allvarifinal:
+        fuco = fu
+        funame = ""
+        ctypesargs = ""
+        numpytypes = []
+        forfunction = []
+        for key, item in v.items():
+            print(key, item)
+            fuco = fuco.replace(key, item)
+            funame += item.replace(" ", "_").strip("_") + "_"
+            for k in d_typesall:
+                if item == k.c:
+                    print(item)
+                    print(k.ct)
+                    if k.ct != "ctypes.c_size_t":
+                        ctypesargs += f"""ctypes.POINTER( {k.ct}),"""
+                    else:
+                        ctypesargs += f""" {k.ct},"""
+
+                    numpytypes.append(k.np)
+                    forfunction.append((k.ct, k.c, k.np, item))
+        funame = funame.strip("_")
+        completefunname = cfunctioname + "_" + funame
+        fuco = fuco.replace("variable_for_function_creation", completefunname)
+        argt = f"""
+            ("{completefunname}",
+            r'''{ctypesargs}''',
+            "bb_",
+            "aa_",
+            None,
+            [{ctypesargs}])
+            """
+        condidion = (
+            "if "
+            + " ".join(
+                [
+                    f"v{n}.dtype == {x[2]} and"
+                    for n, x in enumerate(forfunction)
+                    if x[0] != "ctypes.c_size_t"
+                ]
+            )[:-4]
+            + ":"
+        )
+        ar = "".join([f"v{n}," for n, x in enumerate(numpytypes)])
+        arpo = "".join([f"v{n}po," for n, x in enumerate(numpytypes)])
+        completefunction_with_args = completefunname + f"({arpo})"
+        convertstring = [
+            f"""v{v}np=np.require(v{v}, {x[2]}, ["ALIGNED","C_CONTIGUOUS"])\n        v{v}po=v{v}np.ctypes.data_as(ctypes.POINTER({x[0]}))"""
+            if x[0] != "ctypes.c_size_t"
+            else f"""v{v}po=int(v{v})"""
+            for v, x in enumerate(forfunction)
+        ]
+        convertstring = "\n".join([f"        {x}" for x in convertstring])
+        print(completefunction_with_args)
+        print(argt)
+        print(fuco)
+        allinfos.append(
+            (
+                funame,
+                completefunname,
+                fuco,
+                argt,
+                condidion,
+                ar,
+                completefunction_with_args,
+                convertstring,
+            )
+        )
+
+    allcfunctions = ""
+    for a in allinfos:
+        allcfunctions += (a[2]) + "\n"
+
+    csource = cheader + "\n" + allcfunctions + "\n" + cfooter
+    with open(cpath, mode="w", encoding="utf-8") as f:
+        f.write(csource)
+
+    allfuncvariations = []
+    for a in allinfos:
+        allfuncvariations.append(a[1])
+
+    pythonheader = "import sys\nimport ctypes\nfrom numpy.ctypeslib import ndpointer\nimport configparser\nfrom flexible_partial import FlexiblePartialOwnName\nimport numpy as np\n\n"
+
+    pythonfooter = rf"""
+dllpath = r"{outputdll}"
+cfgfile = r"{cfgfile}"
+lib = ctypes.CDLL(dllpath)
+confignew = configparser.ConfigParser()
+confignew.read(cfgfile)
+funcs = confignew.defaults()
+c_functions = sys.modules[__name__]
+
+def execute_function(f,*args, **kwargs): # create a function
+    f(*args, **kwargs)
+
+allfu = []
+for (fname, descri, function_prefix, functionnormalprefix, restype, argtypes,) in allargtypes:
+    fun = lib.__getattr__(funcs[fname])
+    fun.restype = restype
+    if len(argtypes) != 0:
+        fun.argtypes = argtypes
+    allfu.append((fname, fun))
+    setattr(c_functions, f"{{functionnormalprefix}}{{fname}}", fun)
+    setattr(c_functions, f"{{function_prefix}}{{fname}}", FlexiblePartialOwnName(execute_function, descri, True, fun), )
+
+
+    """
+
+    allargtype = "\nallargtypes = ["
+    for a in allinfos:
+        allargtype += (a[3]) + ","
+
+    allargtype += "]"
+
+    wholepyfun = f"\ndef {cfunctioname}({allinfos[0][5]}):\n\n"
+    for a in allinfos:
+        wholepyfun += "    " + a[4] + "\n"
+        wholepyfun += "" + a[7] + "\n"
+        wholepyfun += f'        c_functions.bb_{f"{a[6]}"}\n        return' + "\n"
+
+    with open(pypath, mode="w", encoding="utf-8") as f:
+        f.write(pythonheader + wholepyfun + allargtype + pythonfooter)
+
+    return allfuncvariations, outputdll, cfgfile, cpath, pypath
+
+
+class CreateCppDllv2:
+    def __init__(
+        self,
+        modulename,
+        cheader,
+        cfooter,
+        cfunctioname,
+        var_variations,
+        dtypemustbeequal,
+        fu,
+    ):
+        (
+            allfuncvariations,
+            outputdll,
+            cfgfile,
+            cpath,
+            pypath,
+        ) = write_source_code_filesv2(
+            modulename,
+            var_variations,
+            dtypemustbeequal,
+            fu,
+            cfunctioname,
+            cheader,
+            cfooter,
+        )
+
+        print("Files written:")
+        print(TC(f"Python import file: {pypath}").bg_black.fg_lightgreen)
+        print(TC(f"C++ file: {cpath}").bg_black.fg_lightgreen)
+        print(TC("If you want/need, you can edit the code before compiling it."))
+        self.outputdll = outputdll
+        self.cfgfile = cfgfile
+        self.cpath = cpath
+        self.modulename = modulename
+        self.allfuncvariations = allfuncvariations
+
+    def compile_cpp_code(
+        self,
+        vcvarsall_bat=r"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat",
+        cl_exe=r"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\Hostx86\x64\cl.exe",
+        link_exe=r"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\Hostx86\x64\link.exe",
+        compilerflags=("/std:c++20", "/Ferelease", "/EHsc", "/MT", "/O2", "/bigobj"),
+    ):
+        compile_cpp_codev2(
+            self.outputdll,
+            self.cfgfile,
+            self.cpath,
+            self.modulename,
+            vcvarsall_bat,
+            cl_exe,
+            link_exe,
+            compilerflags,
+            self.allfuncvariations,
+        )
 
 
 def create_function_variations(
-
     cfunctioname,
     cfunctionname_with_prefix,
     functionvariation1,
@@ -216,7 +521,7 @@ def create_function_variations(
     ignored_dtypes1,
     ignored_dtypes2,
     ignored_dtypes3,
-samedtypes,
+    samedtypes,
     printoutput=False,
 ):
 
@@ -243,11 +548,12 @@ samedtypes,
                 ):
                     continue
                 if samedtypes:
-                    chdi={
-                    "!C_DATA_DTYPE!": dty.c,
-                    "!C_DATA_DTYPE2!": dty2.c,
-                    "!C_DATA_DTYPE3!": dty3.c,}
-                    checkdu=[]
+                    chdi = {
+                        "!C_DATA_DTYPE!": dty.c,
+                        "!C_DATA_DTYPE2!": dty2.c,
+                        "!C_DATA_DTYPE3!": dty3.c,
+                    }
+                    checkdu = []
                     for s in samedtypes:
                         checkdu.append(chdi[s])
                     if len(set(checkdu)) > 1:
@@ -506,6 +812,7 @@ def compile_cpp(
     with open(cfgfile, "w") as f:
         config.write(f)
 
+
 def try_delete_file(path):
     if os.path.exists(path):
         try:
@@ -515,16 +822,17 @@ def try_delete_file(path):
             return False
     return True
 
+
 class CreateCppDll:
     def __init__(
         self,
         cfunctioname,
-        cfunction, samedtypes,
+        cfunction,
+        samedtypes,
         modulename,
         variable_for_function_creation=(
             r"!CT_DATA_DTYPE!_!CT_DATA_DTYPE2!_!C_FUNCTION_NAME!_!CT_DATA_DTYPE3!"
         ),
-
         cheader="""
     #include <iostream>
     #include <stdio.h>
@@ -611,7 +919,8 @@ class CreateCppDll:
             self.vcvarsall_bat,
             self.cl_exe,
             self.link_exe,
-            self.compilerflags, self.samedtypes
+            self.compilerflags,
+            self.samedtypes,
         ) = (
             cfunctioname,
             cfunction,
@@ -625,7 +934,8 @@ class CreateCppDll:
             vcvarsall_bat,
             cl_exe,
             link_exe,
-            compilerflags,samedtypes
+            compilerflags,
+            samedtypes,
         )
         self.functionvariation1 = f"""aa_{variable_for_function_creation}"""
 
@@ -654,7 +964,8 @@ class CreateCppDll:
             replacedict_c_code={},
             ignored_dtypes1=ignored_dtypes1,
             ignored_dtypes2=ignored_dtypes2,
-            ignored_dtypes3=ignored_dtypes3, samedtypes=self.samedtypes
+            ignored_dtypes3=ignored_dtypes3,
+            samedtypes=self.samedtypes,
         )
         self.allfunctionvariations = [
             x[1]["cfunctionname_with_prefix"] for x in self.cpydict.items()
@@ -759,9 +1070,9 @@ for (fname, descri, function_prefix, functionnormalprefix, restype, argtypes,) i
     def compile_cpp_code(self):
         try_delete_file(self.outputdll)
         try_delete_file(self.cfgfile)
-        try_delete_file(self.outputdll[:-3]+'exp')
-        try_delete_file(self.outputdll[:-3] + 'lib')
-        try_delete_file(self.cpath[-3] + 'obj')
+        try_delete_file(self.outputdll[:-3] + "exp")
+        try_delete_file(self.outputdll[:-3] + "lib")
+        try_delete_file(self.cpath[-3] + "obj")
         compile_cpp(
             modulename=self.modulename,
             fnames=self.allfunctionvariations,
@@ -777,6 +1088,4 @@ for (fname, descri, function_prefix, functionnormalprefix, restype, argtypes,) i
             ).bg_black.fg_lightcyan
         )
         sleep(3)
-        try_delete_file(self.cpath[-3] + 'obj')
-
-
+        try_delete_file(self.cpath[-3] + "obj")
